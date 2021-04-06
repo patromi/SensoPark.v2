cc741bfb09a8ab7802669d06ce034eaf2c251fbf
patromi <77615611+patromi@users.noreply.github.com>
2021-04-06 18:32:47 +0200

    Add files via upload

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = 'h'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
     MAIL_USE_TLS = True
