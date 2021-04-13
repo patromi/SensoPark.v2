ffb41f6ed8f34ad2a078f23e4683da4bc082e00d
patromi <77615611+patromi@users.noreply.github.com>
2021-04-13 12:07:10 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_KEY = 'h'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
     MAIL_USE_TLS = True
